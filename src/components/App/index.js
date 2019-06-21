357d8a454a222ac03e493c8c781adbd3b22daf7d
iris paredes <irisparedesjimenez@gmail.com>
2019-06-21 19:15:24 +0200

    add link in PokemonList to pokemon.id... to pokemonDetails

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,8 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import PokemonDetails from '../PokemonDetails';
 
+const totalPokemons = 25;
+const urlApi = `https://pokeapi.co/api/v2/pokemon/?limit=${totalPokemons}`;
 
 class App extends Component {
   constructor(props) {
@@ -13,28 +15,43 @@ class App extends Component {
       pokemonList: [],
       filterBy: ''
     };
-    this.urlApi = 'https://pokeapi.co/api/v2/pokemon/?limit=25';
+    
     this.getUserValue = this.getUserValue.bind(this);
   }
 
   componentDidMount() {
     const localData = JSON.parse(localStorage.getItem('pokeList'));
     if (!localData) {
-      fetchPokemon(this.urlApi)
+      fetchPokemon(urlApi)
         .then(data => {
-        const promises = data.results.map(result => fetchPokemon(result.url));
-        Promise.all(promises).then(responses => {
-          const pokemons = [];
-          for (const response of responses) {
-            pokemons.push(response);
-          }
-          this.setState({ pokemonList: pokemons });
-          localStorage.setItem('pokeList', JSON.stringify(pokemons));
+          data.results.forEach(pokemon => {
+            fetchPokemon(pokemon.url)
+              .then(pokemon => {
+                let pokemonData = pokemon;
+                fetchPokemon(pokemon.species.url)
+                  .then(moreData => {
+                    pokemonData = { ...pokemonData, ...moreData };
+                    this.setState(
+                      prevState => {
+                        return { pokemonList: [...prevState.pokemonList, pokemonData] };
+                      },
+                      () => this.orderAndSaveToLocal(this.state.pokemonList)
+                    );
+                  });
+                });
+              });
         })
         .catch(error => console.log('error', error));
-      });
     } else {
       this.setState({ pokemonList: localData });
+    }
+  }
+
+  orderAndSaveToLocal(array) {
+    if (array.length === totalPokemons) {
+      array.sort((pokemonA, pokemonB) => pokemonA.id - pokemonB.id);
+      this.setState({ pokemonList: array });
+      localStorage.setItem('pokeList', JSON.stringify(array));
     }
   }
 
@@ -48,8 +65,15 @@ class App extends Component {
       <div className="page">
         <div className="page__container">
           <Switch>
-            <Route exact path="/" render={() => <Home filterBy={filterBy} pokemonList={pokemonList} getUserValue={this.getUserValue} />} />
-            <Route path="/pokemon/:id" render={routerProps => <PokemonDetails id={routerProps.match.params.id} pokemonList={pokemonList} />} />
+            <Route exact path="/" render={() => 
+              <Home 
+                filterBy={filterBy} 
+                pokemonList={pokemonList} 
+                getUserValue={this.getUserValue} />} />
+            <Route path="/pokemon/:id" render={routerProps => 
+              <PokemonDetails 
+                id={routerProps.match.params.id} 
+                pokemonList={pokemonList} />} />
           </Switch>
         </div>
       </div>
